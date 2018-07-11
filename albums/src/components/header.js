c07546791c8ff6fe9deaaec39f40e6731a6ae621
lee-philip-n <lee.philip.n@gmail.com>
2018-07-11 14:18:09 -0700

    renamed header.js to Header.js

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -23,7 +23,7 @@ const styles = {
     backgroundColor: '#F8F8F8',
     justifyContent: 'center',
     alignItems: 'center',
-    height: 60,
+    height: '25%',
     paddingTop: 15,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
