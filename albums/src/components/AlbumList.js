94d878b63df4fc419341b87ebba487005f2bb651
lee-philip-n <lee.philip.n@gmail.com>
2018-07-11 12:01:08 -0700

    created AlbumList component

---
+++ albums/src/components/AlbumList.js
@@ -0,0 +1,34 @@
+// Import a library to help create a component
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// Create a component
+const AlbumList = (props) => {
+  return (
+    <View>
+      <Text>Album List</Text>
+    </View>
+  );
+};
+
+//CSS - uses flexbox properties
+const styles = {
+  textStyle: {
+    fontSize: 20
+  },
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
+  },
+};
+
+// Make component available to other parts of the app
+export default AlbumList;
