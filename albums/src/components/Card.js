6db78472ce74ddb8300394ea3f5db862c8b4b5a4
lee-philip-n <lee.philip.n@gmail.com>
2018-07-11 14:17:31 -0700

    created Card component

---
+++ albums/src/components/Card.js
@@ -0,0 +1,12 @@
+// Import a library to help create a component
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+const Card = () => {
+  return (
+    <View>
+    </View>
+  )
+}
+
+export default Card;
