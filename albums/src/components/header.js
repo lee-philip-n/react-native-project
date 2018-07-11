75ef7ce20b13336feee312354a3e0c71b788f5c3
lee-philip-n <lee.philip.n@gmail.com>
2018-07-11 11:43:30 -0700

    added css

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,12 +1,34 @@
 // Import a library to help create a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Create a component
 const Header = () => {
+  const { textStyle, viewStyle } = styles;
+  
   return (
-    <Text>Albums!</Text>
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums!</Text>
+    </View>
   );
+};
+
+const styles = {
+  textStyle: {
+    fontSize: 20
+  },
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
+  },
 };
 
 // Make component available to other parts of the app
