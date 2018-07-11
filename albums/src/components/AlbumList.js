b5b65d918ae3238caa503acdf256a90471868717
lee-philip-n <lee.philip.n@gmail.com>
2018-07-11 12:33:28 -0700

    implemented http request

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,15 +1,35 @@
 // Import a library to help create a component
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
-// Create a component
-const AlbumList = (props) => {
-  return (
-    <View>
-      <Text>Album List</Text>
-    </View>
-  );
-};
+
+// Create a Class Component
+class AlbumList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      albums: [],
+    }
+  }
+
+  //lifecycle event - componentWillMount loads after app (HTTP requests)
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({
+        albums: response.data,
+      }));
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <View>
+        <Text>Album List</Text>
+      </View>
+    );
+  };
+}
 
 //CSS - uses flexbox properties
 const styles = {
